760ea76274862375327bfe13d730e388d9d2cc69
Sercan <sercanyersen@gmail.com>
2017-02-05 17:24:10 +0300

    tost

---
+++ main.js
@@ -0,0 +1,86 @@
+/*document.getElementsByClassname('circle').onclick = function () {
+	document.getElementsByClassname('circle').style.visibility = 'hidden';
+}*/
+
+let clcNu = 2;
+
+
+document.getElementById('1').onclick = function () {
+
+	const cl1 = clcNu
+
+	//let clcNu = cl1;
+
+	if (cl1 % 2 === 0) {
+		document.getElementById('2').style.visibility = 'hidden';
+		clcNu++;
+	} else {
+		document.getElementById('2').style.visibility = 'visible';
+		clcNu++;
+	}
+	
+}
+
+document.getElementById('2').onclick = function () {
+
+	const cl1 = clcNu
+
+	//let clcNu = cl1;
+
+	if (cl1 % 2 === 0) {
+		document.getElementById('5').style.visibility = 'hidden';
+		clcNu++;
+	} else {
+		document.getElementById('5').style.visibility = 'visible';
+		clcNu++;
+	}
+	
+}
+
+document.getElementById('5').onclick = function () {
+
+	const cl1 = clcNu
+
+	//let clcNu = cl1;
+
+	if (cl1 % 2 === 0) {
+		document.getElementById('3').style.visibility = 'hidden';
+		clcNu++;
+	} else {
+		document.getElementById('3').style.visibility = 'visible';
+		clcNu++;
+	}
+	
+}
+
+document.getElementById('3').onclick = function () {
+
+	const cl1 = clcNu
+
+	//let clcNu = cl1;
+
+	if (cl1 % 2 === 0) {
+		document.getElementById('4').style.visibility = 'hidden';
+		clcNu++;
+	} else {
+		document.getElementById('4').style.visibility = 'visible';
+		clcNu++;
+	}
+	
+}
+
+document.getElementById('4').onclick = function () {
+
+	const cl1 = clcNu
+
+	//let clcNu = cl1;
+
+	if (cl1 % 2 === 0) {
+		document.getElementById('1').style.visibility = 'hidden';
+		clcNu++;
+	} else {
+		document.getElementById('1').style.visibility = 'visible';
+		clcNu++;
+	}
+	
+}
