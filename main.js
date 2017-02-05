936b8fdaf0ba122840cbc3ddf4472dda07639750
Sercan <sercanyersen@gmail.com>
2017-02-05 22:24:37 +0300

    tost-maker function is in operation

--- main.js
+++ main.js
@@ -1,77 +1,25 @@
-/*document.getElementsByClassname('circle').onclick = function () {
-	document.getElementsByClassname('circle').style.visibility = 'hidden';
-}*/
-
 let clcNu = 2;
 
+function tostMaker(id) {
 
-
-document.getElementById('1').onclick = function () {
-
-	const cl1 = clcNu
+	const cl1 = clcNu;
 
 	if (cl1 % 2 === 0) {
-		document.getElementById('2').style.visibility = 'hidden';
+		document.getElementById(id).style.visibility = 'hidden';
 		clcNu++;
 	} else {
-		document.getElementById('2').style.visibility = 'visible';
+		document.getElementById(id).style.visibility = 'visible';
 		clcNu++;
 	}
 	
 }
 
-document.getElementById('2').onclick = function () {
+document.getElementById('1').onclick = function(){tostMaker(2);}
 
-	const cl1 = clcNu
+document.getElementById('2').onclick = function(){tostMaker(5);}
 
-	if (cl1 % 2 === 0) {
-		document.getElementById('5').style.visibility = 'hidden';
-		clcNu++;
-	} else {
-		document.getElementById('5').style.visibility = 'visible';
-		clcNu++;
-	}
-	
-}
+document.getElementById('5').onclick = function(){tostMaker(3);}
 
-document.getElementById('5').onclick = function () {
+document.getElementById('3').onclick = function(){tostMaker(4);}
 
-	const cl1 = clcNu
-
-	if (cl1 % 2 === 0) {
-		document.getElementById('3').style.visibility = 'hidden';
-		clcNu++;
-	} else {
-		document.getElementById('3').style.visibility = 'visible';
-		clcNu++;
-	}
-	
-}
-
-document.getElementById('3').onclick = function () {
-
-	const cl1 = clcNu
-
-	if (cl1 % 2 === 0) {
-		document.getElementById('4').style.visibility = 'hidden';
-		clcNu++;
-	} else {
-		document.getElementById('4').style.visibility = 'visible';
-		clcNu++;
-	}
-	
-}
-
-document.getElementById('4').onclick = function () {
-
-	const cl1 = clcNu
-
-	if (cl1 % 2 === 0) {
-		document.getElementById('1').style.visibility = 'hidden';
-		clcNu++;
-	} else {
-		document.getElementById('1').style.visibility = 'visible';
-		clcNu++;
-	}
-	
-}
+document.getElementById('4').onclick = function(){tostMaker(1);}
